3e66665f0c8511dca44ddbba7813d209b521f0bd
Vitaly <kaliummati@gmail.com>
2021-03-19 05:53:20 +0900

    создан метод для обращения к api github для получения трендов репозиториев за сегодняшний день

---
+++ config/githubApiMethods.js
@@ -0,0 +1,10 @@
+export const getTrendsrepositories = async () => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth();
+  const day = today.getDate();
+
+  let response = await fetch(`https://api.github.com/search/repositories?sort=stars&order=desc?page=1&q=created:${year}-${month}-${day}`);
+  let repositories = await response.json();
+  return repositories;
+}
