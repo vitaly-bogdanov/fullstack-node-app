45317be3a8fc00c3b01eeb01fd7cb40a73cd54a2
Vitaly <kaliummati@gmail.com>
2021-03-20 05:45:50 +0900

    доработан метод обращения к api github

--- config/githubApiMethods.js
+++ config/githubApiMethods.js
@@ -1,9 +1,8 @@
-export const getTrendsrepositories = async () => {
+export const getTrendsRepositories = async () => {
   const today = new Date();
   const year = today.getFullYear();
   const month = today.getMonth();
   const day = today.getDate();
-
   let response = await fetch(`https://api.github.com/search/repositories?sort=stars&order=desc?page=1&q=created:${year}-${month}-${day}`);
   let repositories = await response.json();
   return repositories;
