96dbb7e52ddbb381ea3b35e104a2f05f9ba68023
Vitaly <kaliummati@gmail.com>
2021-03-24 03:39:47 +0900

    небольшие правки

--- config/router.js
+++ config/router.js
@@ -1,2 +1,14 @@
 import { Router } from 'express';
 
+// controllers
+import RepositoriesController from '../controllers/RepositoriesController.js';
+
+const router = new Router();
+
+router.get('/repositories', RepositoriesController.index);
+router.get('/repositories/update', RepositoriesController.update);
+router.get('/repositories/get-by-id/:id', RepositoriesController.showById);
+router.get('/repositories/get-by-name/:name', RepositoriesController.showByName);
+router.get('/repositories/delete', RepositoriesController.delete);
+
+export default router;
