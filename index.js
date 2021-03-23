69914e7d5aa0b69076034693e3077507f710bd4f
Vitaly <kaliummati@gmail.com>
2021-03-24 04:27:44 +0900

    небольшие правки

--- index.js
+++ index.js
@@ -3,45 +3,35 @@
 import { cpus } from 'os';
 import cluster from 'cluster';
 import express from 'express';
+import router from './config/router.js';
+import dotenv from 'dotenv';
+import connectToMongo from './config/mongoDatabase.js';
 
+dotenv.config(); // доступ к .env
 const app = express();
+
+const {
+  SERVER_PORT
+} = process.env;
 
 // общие для всех роутов middlewares
 app.use(express.json());
+app.use(router);
 
 if (cluster.isMaster) {
+  // события родительского процесса
+  import('./config/masterProcessEvents.js');
   // занимаем все ядра
   const cpuCount = cpus().length;
   for (let i = 0; i <= cpuCount; i++) {
     cluster.schedulingPolicy = cluster.SCHED_RR;
     cluster.fork();
   }
-  // события родительского процесса
-  import('./config/masterProcessEvents.js');
-  // mongo
-  import('./config/mongoDatabase');
 } else {
-  let server = app.listen(PORT, () => console.log(`Server worked on port ${PORT}, pid: ${process.pid}`));
-  
-  // события дочерних процессов
-  process.on('uncaughtException', (error) => {
-    console.error(error.message);
-    console.error(error.stack);
-    process.exit(1);
-  });
-
-  process.on('SIGINT', () => {
-    console.log(`SIGINT ${process.pid}`);
-    server.close(() => { process.exit(0) });
-  });
-
-  process.on('SIGTERM', () => {
-    console.log(`SIGTERM ${process.pid}`);
-    server.close(() => { process.exit(0) });
-  });
-
-  process.on('SIGUSR2', () => {
-    console.log(`SIGUSR2 ${process.pid}`);
-    server.close(() => { process.exit(1) });
-  });
+  connectToMongo().then(async () => {
+    app.listen(SERVER_PORT, () => {
+      console.log(`Server worked on port ${SERVER_PORT}, pid: ${process.pid}`)
+    });
+    import('./config/childProcessEvent.js');
+  })
 }
