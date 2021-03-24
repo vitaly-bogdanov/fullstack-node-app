20c53ff6f39d0268f3121d5f61824df4d6f5495a
Vitaly <kaliummati@gmail.com>
2021-03-25 01:17:33 +0900

    создано новый хелпер для для обновления записей в бд

--- config/helpers.js
+++ config/helpers.js
@@ -1,6 +1,18 @@
+import Repository from '../models/Repository.js';
+
 export const getAndStartRepositoriesUpdater = () => {
   return setInterval(() => {
     console.log('hello');
     console.log(process.pid);
-  }, 15000)
+  }, 15000);
+}
+
+export const syncRepositoriesWithTransaction = (newRepositories) => {
+  const session = Repository.startSession();
+  const repositories = await session.withTransaction(async () => {
+    await Repository.deleteMany({});
+    return await Repository.create(newRepositories);
+  });
+  session.endSession();
+  return repositories;
 }
