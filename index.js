841d94dd39e2939b9e7672ac1e20a4fbc67d6a46
Vitaly <kaliummati@gmail.com>
2021-03-26 06:16:51 +0900

    небольшие изменения

--- index.js
+++ index.js
@@ -19,19 +19,21 @@ app.use(express.json());
 app.use(router);
 
 if (cluster.isMaster) {
-  // события родительского процесса
-  import('./config/masterProcessEvents.js');
-  // занимаем все ядра
-  const cpuCount = cpus().length;
-  for (let i = 0; i <= cpuCount; i++) {
-    cluster.schedulingPolicy = cluster.SCHED_RR;
-    cluster.fork();
-  }
+  connectToMongo().then(async () => {
+    // события родительского процесса
+    import('./config/masterProcessEvents.js');
+    // занимаем все ядра
+    const cpuCount = cpus().length;
+    for (let i = 0; i <= cpuCount; i++) {
+      cluster.schedulingPolicy = cluster.SCHED_RR;
+      cluster.fork();
+    }
+  });
 } else {
   connectToMongo().then(async () => {
     app.listen(SERVER_PORT, () => {
       console.log(`Server worked on port ${SERVER_PORT}, pid: ${process.pid}`)
     });
     import('./config/childProcessEvent.js');
-  })
+  });
 }
